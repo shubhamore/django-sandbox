3ada2792f70e5c5dc4ec4279e6ad135f039c663d
Shubham More <97827118+shubhamore@users.noreply.github.com>
2024-09-30 15:23:07 +0530

    remove serve from static assets

--- 1. Social Media/myproject/myproject/settings.py
+++ 1. Social Media/myproject/myproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-66994w@4urv3d-dary=h*#8k$jghe@hxbvwxm0xyjo0(qia5+^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
--- 1. Social Media/myproject/myproject/urls.py
+++ 1. Social Media/myproject/myproject/urls.py
@@ -19,11 +19,11 @@ from django.urls import path, include, re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
+# from django.views.static import serve
 
 urlpatterns = [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    # re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    # re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('', views.homepage),
     path('about/', views.aboutpage),
@@ -31,4 +31,4 @@ urlpatterns = [
     path('users/', include('users.urls')),
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
