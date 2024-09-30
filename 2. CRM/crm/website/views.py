96408d813b5cc04577f931a341557ff6223c2608
Shubham More <97827118+shubhamore@users.noreply.github.com>
2024-09-30 18:59:57 +0530

    added view records individualy and together in a table

--- 2. CRM/crm/website/views.py
+++ 2. CRM/crm/website/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
     if request.method=='POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -17,7 +19,7 @@ def home(request):
             messages.error(request, 'Invalid credentials')
             return redirect('home')
     else:
-        return render(request, 'home.html')
+        return render(request, 'home.html',{'records':records})
     
 def logout_user(request):
     logout(request)
@@ -38,3 +40,11 @@ def register_user(request):
     else:
         form = SignUpForm()
     return render(request, 'register.html', {'form': form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':record})
+    else:
+        messages.error(request, 'Please login first')
+        return redirect('home')
