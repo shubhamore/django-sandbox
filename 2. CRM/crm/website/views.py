a3c909732838d05f760f12e91c5f2fb3a80d79e1
Shubham More <97827118+shubhamore@users.noreply.github.com>
2024-09-30 19:33:45 +0530

    implemented create, delete and update of record

--- 2. CRM/crm/website/views.py
+++ 2. CRM/crm/website/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
+
 
 # Create your views here.
 def home(request):
@@ -45,6 +46,42 @@ def customer_record(request, pk):
     if request.user.is_authenticated:
         record = Record.objects.get(id=pk)
         return render(request, 'record.html', {'customer_record':record})
+    else:
+        messages.error(request, 'Please login first')
+        return redirect('home')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        record = Record.objects.get(id=pk)
+        record.delete()
+        messages.success(request, 'Record was deleted')
+        return redirect('home')
+    else:
+        messages.error(request, 'Please login first')
+        return redirect('home')
+    
+def add_record(request):
+    form=AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method=='POST':
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Record was added')
+                return redirect('home')
+        return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.error(request, 'Please login first')
+        return render(request, 'home.html', {'form':form})
+
+def update_record(request,pk):
+    if request.user.is_authenticated:
+        record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Record was updated')
+            return redirect('home')
+        return render(request, 'update_record.html', {'form':form})
     else:
         messages.error(request, 'Please login first')
         return redirect('home')
