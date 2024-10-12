035023d1bb295534dec6fe3a30386ce7e04eb00c
Shubham More <97827118+shubhamore@users.noreply.github.com>
2024-10-13 00:37:36 +0530

    added pagination, filter, search, and completed field in task model

--- 4. Todo API/todo_api/task/views.py
+++ 4. Todo API/todo_api/task/views.py
@@ -1,12 +1,16 @@
-# task/views.py
 from rest_framework import generics, permissions
 from .models import Task
 from .serializers import TaskSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 # View to list and create tasks
 class TaskListCreateView(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['title']
+    filterset_fields = ['completed']
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)  
--- 4. Todo API/todo_api/todo_api/settings.py
+++ 4. Todo API/todo_api/todo_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'django_filters',
     'user',
     'task',
 ]
@@ -133,6 +134,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+    ),
 }
 
 from datetime import timedelta
