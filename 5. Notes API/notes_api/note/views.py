7a939929c81eea8f668d90f28f8db5bffaace6a2
Shubham More <97827118+shubhamore@users.noreply.github.com>
2024-10-13 22:16:17 +0530

    added export in csv and json format

--- 5. Notes API/notes_api/note/views.py
+++ 5. Notes API/notes_api/note/views.py
@@ -5,6 +5,10 @@ from .serializers import NoteSerializer
 from .models import Note
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from rest_framework.views import APIView
+import csv
+from django.http import HttpResponse
+
 # Create your views here.
 
 class NoteListCreateView(ListCreateAPIView):
@@ -28,3 +32,25 @@ class NoteRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         return Note.objects.filter(owner=self.request.user)
     
+class NoteExportNotesCSV(APIView):
+    permission_classes=[IsOwner]
+    def get(self,request):
+        notes = Note.objects.filter(owner=request.user)
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="notes.csv"'
+
+        writer = csv.writer(response)
+        writer.writerow(['Title', 'Description', 'Created At', 'Last Modified'])  
+
+        for note in notes:
+            writer.writerow([note.title, note.description, note.created_at, note.last_modified])
+
+        return response
+    
+class NoteExportNotesJSON(APIView):
+    permission_classes=[IsOwner]
+    def get(self,request):
+        notes = Note.objects.filter(owner=request.user)
+        serializer = NoteSerializer(notes, many=True)
+        return Response(serializer.data, content_type='application/json')
