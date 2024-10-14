695225216b9ff141409d61df183dca2cce41ab51
Shubham More <97827118+shubhamore@users.noreply.github.com>
2024-10-14 19:09:05 +0530

    renamed note-remove-self view to remove duplication of names

--- 5. Notes API/notes_api/note/urls.py
+++ 5. Notes API/notes_api/note/urls.py
@@ -4,7 +4,7 @@ from .views import NoteListCreateView, NoteRetrieveUpdateDestroyView, NoteExport
 urlpatterns = [
     path('', NoteListCreateView.as_view(), name="note-list-create"),
     path('<int:pk>/', NoteRetrieveUpdateDestroyView.as_view(), name="note-detail"),
-    path('<int:pk>/remove-self/', NoteRemoveSelfFromSharedView.as_view(), name="note-detail"),
+    path('<int:pk>/remove-self/', NoteRemoveSelfFromSharedView.as_view(), name="note-remove-self"),
     path('export/csv/',NoteExportNotesCSV.as_view(),name='export-csv'),
     path('export/json/',NoteExportNotesJSON.as_view(),name='export-json'),
 ]
